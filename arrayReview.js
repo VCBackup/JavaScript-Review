65a207b6aacf85dca5835661f9a96d2772bf1ad2
VCBackup <dmoore1525@gmail.com>
2017-05-15 20:10:17 -0600

    First initial problems solved in the arrayReview.

--- arrayReview.js
+++ arrayReview.js
@@ -3,7 +3,10 @@ var threeItems = [1,2,3];
 //alert the result of your function
 
   //code here
-
+function last(arr){
+  var lastDigit = arr.length -1;
+  alert(arr[lastDigit]);
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -14,20 +17,36 @@ var threeItems = [1,2,3];
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
-
+function looping(arr){
+  for (var i = arr.length +1; i > arr.length; i--){
+    var moduloValue = arr[i] % 2;
+    if (moduloValue == 0){
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a
+//random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
-
+function randomStuff(randomNum, arr){
+  for (var i = 0; i < arr.length; i++){
+    if (randomNum == arr[i]){
+      alert(true);
+    }
+  }
+  alert(false);
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
